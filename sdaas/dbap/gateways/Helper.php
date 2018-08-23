75152cefc4ea83f712b8620d930835917987b7f2
Yassine Ouahidi <marvindoufresn@gmail.com>
2018-08-23 16:08:59 +0200

    Gateway ready

---
+++ sdaas/dbap/gateways/Helper.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * A collection of custom filters to be used as callback with php data filtering functions
+ *  - to allow empty values  if the data is invalid filter MUST returns null
+ *  - to deny empty values if the data is invalid filter MUST returns false
+ */
+class Helper {
+    
+    const UNITS_REGEXP = '/(hundred|thousand|million|billion)/i';
+
+    public static function getAltLabel($label){
+        $arrayMinisteri = array(
+            "MINISTERO DEGLI AFFARI ESTERI E DELLA COOPERAZIONE INTERNAZIONALE" => "Affari Esteri",
+            "MINISTERO DEI BENI E DELLE ATTIVITA' CULTURALI E DEL TURISMO" => "Beni e attività culturali e Turismo",
+            "MINISTERO DEL LAVORO E DELLE POLITICHE SOCIALI" => "Lavoro e Politiche Sociali",
+            "MINISTERO DELLA DIFESA" => "Difesa",
+            "MINISTERO DELLA GIUSTIZIA" => "Giustizia",
+            "MINISTERO DELLA SALUTE" => "Salute",
+            "MINISTERO DELL'AMBIENTE E DELLA TUTELA DEL TERRITORIO E DEL MARE" => "Ambiente e Territorio",
+            "MINISTERO DELLE INFRASTRUTTURE E DEI TRASPORTI" => "Infrastrutture e Trasporti",
+            "MINISTERO DELLE POLITICHE AGRICOLE ALIMENTARI E FORESTALI" => "Politiche Agricole Alimentari",
+            "MINISTERO DELL'ECONOMIA E DELLE FINANZE" => "Economia e Finanze",
+            "MINISTERO DELL'INTERNO" => "Interno",
+            "MINISTERO DELL'ISTRUZIONE, DELL'UNIVERSITA' E DELLA RICERCA" => "Istruzione",
+            "MINISTERO DELLO SVILUPPO ECONOMICO" => "Sviluppo Economico"
+        );
+        return $arrayMinisteri[$label];
+    }
+
+        /**
+     * escape double quotes, backslash and new line
+     * empty allowed
+     */
+    public static function FILTER_SANITIZE_TURTLE_STRING($value)
+    {
+        $value = preg_replace('/\\\\/', '\\\\\\\\', $value);    // escape backslash
+        $value = preg_replace('/\r?\n|\r/', '\\n', $value);  // newline 
+        $value = preg_replace('/"/', '\\"', $value);        // escape double quote
+        
+        return $value?:null;
+    }
+    
+}
---
+++ sdaas/dbap/gateways/dbap-legge-di-bilancio.php
@@ -0,0 +1,115 @@
+<?php
+//dataset-afcec986-9a57-4eae-abfa-543a4f043c5e
+if(!isset($argv[1])){
+    echo "You need to provide the dataset id";
+    die();
+}
+
+require_once('Helper.php');
+
+$bdapDatasetId = $argv[1];
+$bdapDatasetUri = "http://data.budget.g0v.it/resource/$bdapDatasetId";
+$taxonomyUri = "<$bdapDatasetUri#taxonomy>";
+//ARRAY FOR VALUES
+$a = "amministrazione"; $m = "missione"; $p = "programma"; $az = "azione"; $c = "capitolo"; $b = "budget";
+$match = array(
+    $a => 2,
+    $m => 13,
+    $p => 15,
+    $az => 19,
+    $c => 6,
+    $b => 20
+);
+//PREFIXES
+printf('@prefix g0v: <http://data.budget.g0v.it/g0v-budget/v1> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> . 
+@prefix skos:     <http://www.w3.org/2004/02/skos/core#> . 
+@prefix dcat:      <http://www.w3.org/ns/dcat#> . 
+@prefix dct:      <http://purl.org/dc/terms/> . 
+@prefix foaf:     <http://xmlns.com/foaf/0.1/> . 
+@prefix interval: <http://reference.data.gov.uk/def/intervals/> . 
+@prefix qb:       <http://purl.org/linked-data/cube#> . 
+@prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix sdmx-dimension:  <http://purl.org/linked-data/sdmx/2009/dimension#> .
+@prefix sdmx-measure:    <http://purl.org/linked-data/sdmx/2009/measure#> . 
+@prefix sdmx-attribute:  <http://purl.org/linked-data/sdmx/2009/attribute#> .
+@prefix rdfs: <urn:g0v:budget:example:resource:>.
+');
+
+//TAXONOMY
+echo"
+$taxonomyUri a g0v:BudgetTaxonomy, skos:ConceptScheme;
+    dct:src <$bdapDatasetUri> ; 
+    dct:title \"Tassonomia capitoli di spesa per amministrazioni\"@it;
+    dct:description \"Tassonomia amministrazioni/missioni/programmi/azioni/capitoli ricavata dalla legge di bilancio\"@it
+    ."
+;
+echo "\n";
+//skips header
+fgets(STDIN);
+while ($rawdata = fgetcsv(STDIN, 2048, ';')) {
+    $amministrazione = $rawdata[$match[$a]];
+    $missione = $rawdata[$match[$m]];
+    $programma = $rawdata[$match[$p]];
+    $azione = $rawdata[$match[$az]]; 
+    $capitolo = $rawdata[$match[$c]];
+    $budget = $rawdata[$match[$b]];
+
+    //Codes then used in notation and uri construction
+    $a_code = md5($amministrazione);
+    $m_code = md5($amministrazione . $missione);
+    $p_code = md5($amministrazione . $missione . $programma);
+    $az_code = md5($amministrazione . $missione . $programma . $azione);
+    $c_code = md5($amministrazione . $missione . $programma . $azione . $capitolo);
+    
+    //Uris
+    $a_uri = "<$bdapDatasetUri#{$a}_$a_code>";
+    $m_uri = "<$bdapDatasetUri#{$m}_$m_code>";
+    $p_uri = "<$bdapDatasetUri#{$p}_$p_code>";
+    $az_uri = "<$bdapDatasetUri#{$az}_$az_code>";
+    $c_uri = "<$bdapDatasetUri#{$c}_$c_code>";
+    $b_uri = "<$bdapDatasetUri#{$b}_" . uniqid() .">";
+
+    //AMMINISTRAZIONE
+    printSingleLevel($a_uri, $a_code, Helper::FILTER_SANITIZE_TURTLE_STRING($amministrazione), Helper::FILTER_SANITIZE_TURTLE_STRING(Helper::getAltLabel($amministrazione)), "");
+    //MISSIONE
+    printSingleLevel($m_uri, $m_code, Helper::FILTER_SANITIZE_TURTLE_STRING($missione) , Helper::FILTER_SANITIZE_TURTLE_STRING($missione), $a_uri);
+    //PROGRAMMA
+    printSingleLevel($p_uri, $p_code, Helper::FILTER_SANITIZE_TURTLE_STRING($programma) , Helper::FILTER_SANITIZE_TURTLE_STRING($programma), $m_uri);
+    //AZIONE
+    printSingleLevel($az_uri, $az_code, Helper::FILTER_SANITIZE_TURTLE_STRING($azione), Helper::FILTER_SANITIZE_TURTLE_STRING($azione), $p_uri); 
+    //CAPITOLO
+    printSingleLevel($c_uri, $c_code, Helper::FILTER_SANITIZE_TURTLE_STRING($capitolo), Helper::FILTER_SANITIZE_TURTLE_STRING($capitolo), $az_uri);
+    //BUDGET
+    printBudget($b_uri, $c_uri, Helper::FILTER_SANITIZE_TURTLE_STRING($capitolo), Helper::FILTER_SANITIZE_TURTLE_STRING($budget));
+    //TOP CONCEPT
+    printf("\n%s skos:hasTopConcept %s .\n", $taxonomyUri, $a_uri);
+}
+
+function printBudget($uri, $subjectUri, $subjectLabel, $value){
+    $bdapDatasetUri = $GLOBALS['bdapDatasetUri'];
+    printf("%s a g0v:Budget, qb:Observation ;
+        g0v:subject %s ;
+        qb:dataSet  <%s>;
+        rdfs:comment \"Valore riferito al capitolo di spesa %s\"@it ;
+        g0v:obsValue %s ."
+        ,$uri, $subjectUri, $bdapDatasetUri, $subjectLabel,$value);
+}
+
+function printSingleLevel($uri, $notation, $prefLabel, $altLabel, $broaderUri){
+    $taxonomyUri = $GLOBALS['taxonomyUri'];
+    printf('%s a skos:Concept ;
+        skos:notation "%s" ;
+        skos:prefLabel "%s"@it ;
+        skos:altLabel "%s"@it ;
+        skos:inScheme %s',
+        $uri, $notation, $prefLabel, $altLabel, $taxonomyUri);
+    
+    if(!empty($broaderUri)){
+        printf(";\n\tskos:broader %s", $broaderUri);
+    }
+    printf(".\n");
+}
+
+
+?>
