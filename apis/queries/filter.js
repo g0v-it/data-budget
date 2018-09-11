2c7a6ab263b61a7edcdf78958d86e1415e747f3f
Miah Mohd Ehtesham <miahmohd98@gmail.com>
2018-09-11 20:39:54 +0000

    Update filter.js

--- apis/queries/filter.js
+++ apis/queries/filter.js
@@ -3,7 +3,7 @@
 module.exports = (top_partition_label, second_partition_label, group_label) => {
 	return ({
 		query : `
-		PREFIX : <urn:local:g0v:api:v1:>
+		PREFIX : <http://data.budget.g0v.it/g0v-ap-api/v1#>
 		SELECT ?${group_label} (SUM (?account_amount) AS ?amount)
 		WHERE {
      		?accountUri a :ReferenceAccount;
