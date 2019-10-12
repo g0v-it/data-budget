53411d8d2253bd1fb8b0b01e963ea843cc58d9bf
Configuration Manager <admin@linkeddata.center>
2019-10-12 20:43:46 +0200

    refactory

--- gateways/Helper.php
+++ gateways/Helper.php
@@ -8,44 +8,57 @@
 class Helper {
     
     const UNITS_REGEXP = '/(hundred|thousand|million|billion)/i';
-
-    public static function getAltLabel($label){
-        $arrayMinisteri = array(
-            "MINISTERO DEGLI AFFARI ESTERI E DELLA COOPERAZIONE INTERNAZIONALE" => "Affari Esteri",
-            "MINISTERO DEI BENI E DELLE ATTIVITA' CULTURALI E DEL TURISMO" => "Beni e attività culturali e Turismo",
-            "MINISTERO DEL LAVORO E DELLE POLITICHE SOCIALI" => "Lavoro e Politiche Sociali",
-            "MINISTERO DELLA DIFESA" => "Difesa",
-            "MINISTERO DELLA GIUSTIZIA" => "Giustizia",
-            "MINISTERO DELLA SALUTE" => "Salute",
-            "MINISTERO DELL'AMBIENTE E DELLA TUTELA DEL TERRITORIO E DEL MARE" => "Ambiente e Territorio",
-            "MINISTERO DELLE INFRASTRUTTURE E DEI TRASPORTI" => "Infrastrutture e Trasporti",
-            "MINISTERO DELLE POLITICHE AGRICOLE ALIMENTARI E FORESTALI" => "Politiche Agricole Alimentari",
-            "MINISTERO DELL'ECONOMIA E DELLE FINANZE" => "Economia e Finanze",
-            "MINISTERO DELL'INTERNO" => "Interno",
-            "MINISTERO DELL'ISTRUZIONE, DELL'UNIVERSITA' E DELLA RICERCA" => "Istruzione",
-            "MINISTERO DELLO SVILUPPO ECONOMICO" => "Sviluppo Economico"
-        );
-        return isset($arrayMinisteri[$label]) ? $arrayMinisteri[$label] : strtolower($label);
-    }
-
+    const ARTICOLI_REGEX = '/\b(il|lo|l\'|i|gli|la|le|un|uno|una|un\')\b/i'; // matches articoli determinativi e indeterminativi
+    const PREPOSIZIONI_PROPRIE_REGEX = '/\b(di|a|da|in|con|su|per|tra|fra)\b/i'; // matches preposizioni proprie
+    const PREPOSIZIONI_IMPROPRIE_REGEX = '/\b(davanti|dietro|dopo|fuori|lontano|lungo|mediante|prima|sopra|sotto)\b/i'; // matches preposizioni improprie
+    const PREPOSIZIONI_ARTICOLATE_REGEX = '/\b(Del|Dello|Della|Dell\'|Dei|Degli|Delle|Al|Allo|Alla|Ai|Agli|Alle|All\'|Dal|Dallo|Dalla|dall\'|Dai|Dagli|Dalle|Nel|Nello|Nella|Nell\'|Nei|Negli|Nelle|Sul|Sullo|Sulla|Sull\'|Sui|Sugli|Sulle)\b/i';
+ 
     /**
      * questa funzione genera un codice univoco a partire da una stringa
      * numeri, spazi, segni di interpunzione e vocali sono rimossi
-     * la stringa viene convertita in maiuscolo, 
+     * la stringa viene convertita in maiuscolo,
      * solo i rimanenti caratteri alfabetici sono significativi.
-     * 
-     * La funzione viene computta per estrarre un identificatvo d auna descrizione testuale
+     *
+     * La funzione viene computata per estrarre un identificativo da una descrizione testuale che può subire minime modifiche lessicali (da ignorare)
      */
-    public static function getUri($string){
-        $string = strtoupper($string);
-        $string = preg_replace('/\s+/', '', $string);
-        $string = preg_replace('/[^A-Z]/', '', $string);
-        $string = preg_replace('/[AEIOU]/', '', $string);
-        $uri = CRC32($string);
-        return $uri;
+    public static function STRING_SIGNATURE($string)
+    {   
+        // move to upper
+        $string = strtoupper($string);                            
+        
+        // remove any abbreviations smaller than 2 caracter
+        $string = preg_replace("/[a-z]{1-2}\./i",'', $string);
+        
+        // ignore attributes
+        $string = preg_replace(self::ARTICOLI_REGEX,'', $string);
+        $string = preg_replace(self::PREPOSIZIONI_PROPRIE_REGEX,'', $string);
+        $string = preg_replace(self::PREPOSIZIONI_IMPROPRIE_REGEX,'', $string);
+        $string = preg_replace(self::PREPOSIZIONI_ARTICOLATE_REGEX,'', $string);
+        
+        // remove other misleading short tockens
+        $string = preg_replace('/\b(e|che)\b/i','', $string);
+
+        
+        // normalize separators
+        $string = preg_replace('/[\s\'",;\.\-:\/]/', '_', $string);
+        $string = preg_replace('^/^', '_', $string);
+        $string = preg_replace('/__/', '_', $string);
+        $string = trim($string,'_');
+        
+        // remove all non alphanumeric characters
+        $string = preg_replace('/[^A-Z0-9_]/', '', $string);
+        
+        // remove vowels
+        $string = preg_replace("/[AEIOU]*/",'', $string);
+        
+        
+        // ignore numbers
+        $string = preg_replace("/\b\d+\b/",'', $string);
+        
+        return  CRC32($string);
     }
 
-        /**
+     /**
      * escape double quotes, backslash and new line
      * empty allowed
      */
